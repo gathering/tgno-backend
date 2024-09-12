d7a171d577d656b13a52ec13aec5b1cee8bf5ce0
Ole Mathias Heggem <olemathias.aa.heggem@gmail.com>
2024-09-12 12:18:30 +0200

    fix: add CSRF_TRUSTED_ORIGINS

--- tgno/settings/production.py
+++ tgno/settings/production.py
@@ -5,6 +5,7 @@ from .base import *
 DEBUG = False
 SECRET_KEY = env("SECRET_KEY")
 ALLOWED_HOSTS = env("ALLOWED_HOSTS", "localhost").split(",")
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS", "http://localhost").split(",")
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 LOGGING = {
