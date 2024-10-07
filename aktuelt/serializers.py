4a85a72281aee6ce28230a2ad16398c77a4289f5
niccofyren <niccofyren@gmail.com>
2024-10-07 19:43:27 +0200

    feat: add image sizes to news images (#22)

--- aktuelt/serializers.py
+++ aktuelt/serializers.py
@@ -2,6 +2,31 @@ from rest_framework.fields import Field
 from wagtail.images.api.fields import ImageRenditionField
 
 
+class NewsImageSerializer(Field):
+    def image_size(self, size, value):
+        image = ImageRenditionField(size).to_representation(value)
+        return {"url": image["full_url"], "width": image["width"], "height": image["height"]}
+
+    def to_representation(self, value):
+        data = {
+            "id": value.id,
+            "title": value.title,
+            "alt": value.title,
+            "type": "foto",
+            "focus": "center",
+            "author": None,
+            "sizes": {
+                "thumbnail": self.image_size("fill-150x150", value),
+                "small": self.image_size("max-300x300", value),
+                "medium": self.image_size("max-700x700", value),
+                "large": self.image_size("max-1600x1600", value),
+                "extra-large": self.image_size("max-3200x3200", value),
+            },
+        }
+        data["url"] = data["sizes"]["large"]["url"]
+        return data
+
+
 class ContributorsSerializer(Field):
     def to_representation(self, news_page_contributors):
         return [
@@ -22,7 +47,7 @@ class NewsPageGallerySerializer(Field):
         return [
             # 100% guarantee there are better ways to do this
             # TODO: Replace with generic and reusable serializer
-            ImageRenditionField("fill-100x100").to_representation(gallery_image.image)
+            NewsImageSerializer().to_representation(gallery_image.image)
             for gallery_image in value.all()
         ]
 
