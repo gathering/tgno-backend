606b26de6ae8f3c188fad5e4205aa378e3ed8e14
Nicolai Rosdahl Tellefsen <niccofyren@gmail.com>
2025-02-07 22:03:55 +0100

    feat: list all relevant praktisk models on api route

--- praktisk/api.py
+++ praktisk/api.py
@@ -2,17 +2,34 @@ from django.conf import settings
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
+from wagtail.models import Page
 
-from .models import InfoPage
+from .models import FaqPage, InfoPage
 
 api_router = WagtailAPIRouter("praktiskapi")
 
 
 class InfoPagesAPIViewSet(PagesAPIViewSet):
-    model = InfoPage
     renderer_classes = [
         JSONRenderer,
     ]
+
+    meta_fields = PagesAPIViewSet.meta_fields + ["url"]
+
+    def get_listing_default_fields(self):
+        return [
+            "body",
+            "intro",
+            "id",
+            "slug",
+            "title",
+            "type",
+            "url",
+        ]
+
+    def get_base_queryset(self):
+        # Only allow page types we own
+        return Page.objects.all().type(InfoPage, FaqPage)
 
 
 if settings.DEBUG:
